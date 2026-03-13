a35e47ed24495f25072086c645f9a1ac46f4a6e2
b4nn3d0124 <b4nn3d.0124@gmail.com>
2026-03-13 14:09:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG={
 API_URL:"https://script.google.com/macros/s/AKfycbyhrz6pRuTXTBLwnWSqr_DJhkKK8g9fna8y5uJqf65WieI4PVgWQV_BDsnaMd-CWGSBYA/exec",
 GOOGLE_CLIENT_ID:"955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
-COMPANY_DOMAIN:"gmail.com"
+COMPANY_DOMAIN:"wilsonworks.ph"
 }
