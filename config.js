f52ec021f5edffc1a5517304e791bae20c5c8056
b4nn3d0124 <b4nn3d.0124@gmail.com>
2026-03-13 15:38:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const CONFIG={
-API_URL:"https://script.google.com/macros/s/AKfycbyhrz6pRuTXTBLwnWSqr_DJhkKK8g9fna8y5uJqf65WieI4PVgWQV_BDsnaMd-CWGSBYA/exec",
+API_URL:"https://script.google.com/macros/s/AKfycbzTSeMMHuhvddUcEBmHNCA3HBkuzvZOHfR6wqFRVcrdiy0Q85U6FRjY9RO2reBHH-2wzw/exec",
 GOOGLE_CLIENT_ID:"955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
 COMPANY_DOMAIN:"gmail.com"
 }
